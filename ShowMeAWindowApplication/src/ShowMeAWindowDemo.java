a18653b0e944cc84d33ddf1cd73a501691d4d94d
Sasha <sasha.strogo@gmail.com>
2018-12-21 17:59:49 +0700

    Начало изучения учебника Васильева А.Н. Написание первой программы на Java.

---
+++ ShowMeAWindowApplication/src/ShowMeAWindowDemo.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+class ShowMeAWindowDemo{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Первая программа на Java");
+    }
+}
