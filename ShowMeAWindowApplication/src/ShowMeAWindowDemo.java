28245a336ceed2f0572fe17d850a93c2e18c4c46
Sasha <sasha.strogo@gmail.com>
2018-12-30 23:36:41 +0700

    Урок 2. Убрала пикторгамму вообще.

--- ShowMeAWindowApplication/src/ShowMeAWindowDemo.java
+++ ShowMeAWindowApplication/src/ShowMeAWindowDemo.java
@@ -3,6 +3,6 @@ class ShowMeAWindowDemo{
     public static void main(String[] args){
         String title="Сообщение";
         String text="Продолжаем изучать Java";
-        JOptionPane.showMessageDialog(null,text,title,JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(null,text,title,JOptionPane.PLAIN_MESSAGE);
     }
 }
