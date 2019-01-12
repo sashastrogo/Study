28b84dcf19e13fea54dbbfff4a076ec8ba2f6f55
Sasha <sasha.strogo@gmail.com>
2019-01-12 20:30:46 +0700

    Урок 6.Создание окна с заданным названием и заданное сообщение в этом окне. Доделала то, что запарола в прошлый раз.

--- ShowMeWindowsApplication/src/ShowMeWindowsApplication.java
+++ ShowMeWindowsApplication/src/ShowMeWindowsApplication.java
@@ -3,8 +3,8 @@ class ShowMeWindowsDemo{
     public static void main(String[]args){
         String title;
         String text;
-        title=JOptionPane.showInputDialog(null,"Привет:","Привет",JOptionPane.WARNING_MESSAGE);
-        text=JOptionPane.showInputDialog(null,"Как дела:","Хорошо",JOptionPane.PLAIN_MESSAGE);
+        title=JOptionPane.showInputDialog(null,"Имя окна:","Название",JOptionPane.WARNING_MESSAGE);
+        text=JOptionPane.showInputDialog(null,"Текст сообщения:","Содержание",JOptionPane.PLAIN_MESSAGE);
         JOptionPane.showMessageDialog(null,text,title,JOptionPane.INFORMATION_MESSAGE);
     }
 }
