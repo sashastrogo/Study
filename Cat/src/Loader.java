3ae73112f33004c047ff606a8bafde2806519aa6
sashastrogo <sasha.strogo@gmail.com>
2019-03-17 21:58:51 +0700

    3.2 new cats
    Создание кошек и запрос свойств.

--- Cat/src/Loader.java
+++ Cat/src/Loader.java
@@ -3,10 +3,37 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat murzik = new Cat();
+
+        Cat vaska = new Cat();
+
+        Cat tapok = new Cat();
+
+        Cat barsik = new Cat();
+
+        Cat murka = new Cat();
 
 
+        System.out.println("murzik weight = " + murzik.getWeight());
+        System.out.println("murzik say:");
+        murzik.meow();
+        System.out.println("murzik weight = " + murzik.getWeight());
 
-        System.out.println(cat.getStatus());
+        System.out.println("vaska - " + vaska.getStatus());
+
+        System.out.println(tapok.getWeight());
+        System.out.println("Now tapok is " + tapok.getStatus());
+        tapok.feed(1000.0);
+        System.out.println(tapok.getWeight());
+        System.out.println("Now tapok is " + tapok.getStatus());
+
+        System.out.println(barsik.getWeight());
+        barsik.feed(10000.0);
+        System.out.println(barsik.getWeight());
+        System.out.println("Sorry, your's barsik is " + barsik.getStatus());
+
+        System.out.println(murka.getWeight());
+        murka.drink(1.0);
+        System.out.println(murka.getWeight());
     }
 }
